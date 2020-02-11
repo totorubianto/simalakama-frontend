4316d8aad0486e6cf127714082d8ab3183d191d6
Toto <toto.rubianto.17@gmail.com>
2020-02-11 14:11:57 +0700

    convert pascal case to camel case

--- src/components/global/style/ButtonGroup.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-export const ButtonGroup = styled.div`
-    display: flex;
-    button {
-        margin-right: 10px;
-    }
-`;
--- src/components/home/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface Props {}
-
-const Home: React.FC<Props> = () => {
-    return <div>hello</div>;
-};
-
-export default Home;
